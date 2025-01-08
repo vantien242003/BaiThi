432cd39b6b8b218ffee7fea7b86b6ea3fbb8232a
vantien242003 <vantiennguyen243@gmail.com>
2025-01-08 13:03:44 -0500

    first

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,55 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: %i[ show edit update destroy ]
+# hien thi
+  def index
+    @products = Product.all
+  end
+# show id
+  def show
+    @product = Product.find(params[:id])
+  end
+# tao san pham moi
+  def new
+    @product = Product.new
+  end
+# else
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # sua san pham
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+# cap nhat san pham
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+# xoa san pham
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.expect(product: [ :name ])
+    end
+
+end
